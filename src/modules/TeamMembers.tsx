0e915e7fdbea5ffbd3f7096b5d22e056b0005431
Umarfarook <ujuj2451@gmail.com>
2025-02-23 22:20:04 +0530

    Photos updated

--- src/modules/TeamMembers.tsx
+++ src/modules/TeamMembers.tsx
@@ -6,10 +6,10 @@ import img2 from '../assets/TeamMembers/Devaki.png';
 import img3 from '../assets/TeamMembers/sherin.png';
 import img4 from '../assets/TeamMembers/harini.jpg';
 import img5 from '../assets/TeamMembers/karthi.png';
-import img6 from '../assets/TeamMembers/viji.jpeg';
+import img6 from '../assets/TeamMembers/viji.png';
 import img7 from '../assets/TeamMembers/nivi.png'
 import img8 from '../assets/TeamMembers/vijay.png';
-import img9 from '../assets/TeamMembers/pavi.jpg';
+import img9 from '../assets/TeamMembers/pavi.png';
 import './TeamMembers.scss';
 
 const teamMembers = [
